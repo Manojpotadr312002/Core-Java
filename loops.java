cfc031890a8779f107bc024163c81a798b4a1c88
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-04-27 10:41:51 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,38 @@
+package ITPreneur;
+
+public class loops {
+
+	public static void main(String[] args) {
+		
+		
+		// for loop
+		System.out.println("------- for loop -------");
+		System.out.println("Print 1 to 10 numbers");
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		
+		// while loop
+		System.out.println("------- while loop -------");
+		System.out.println("Print 1 to 10 numbers");
+		int j=1;
+			while(j<=10) {
+				System.out.println(j);
+				j++;
+			}
+				
+		
+		// do-while loop
+		System.out.println("------- do-while loop -------");
+		System.out.println("Print 1 to 10 numbers");
+		int k=1;
+			do {
+				System.out.println(k);
+				k++;
+			}
+		while(k<=10);
+	}
+
+}
+
