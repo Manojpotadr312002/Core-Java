a356969f95dbc81472a6cb3d1c80294ec5a98a17
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-04-28 09:43:07 +0530

    Create Tables.java

---
+++ Tables.java
@@ -0,0 +1,12 @@
+class Tables {
+    public static void main(String[] args)
+    {
+      
+        int n = 7;
+        for (int i = 1; i <= 10; i++) {
+            
+            System.out.println(N + " * " + i + " = "
+                               + N * i);
+        }
+    }
+}
