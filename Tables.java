0e3bd1d4e34b4960671ed263b343a93e1fdc7af7
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-04-29 10:38:21 +0530

    Tables.java

--- Tables.java
+++ Tables.java
@@ -1,12 +1,38 @@
-class Tables {
-    public static void main(String[] args)
-    {
-      
-        int n = 7;
-        for (int i = 1; i <= 10; i++) {
-            
-            System.out.println(N + " * " + i + " = "
-                               + N * i);
-        }
-    }
+public class Tables {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------ Using for loop ------");
+		System.out.println("------ Print the table of 5 ------");
+		int n=5;
+		for(int i=1; i<=10; i++) {
+			System.out.println(n*i);
+		}
+		System.out.println();
+		
+		
+		
+		System.out.println("------ Using while loop ------");
+		System.out.println("------ Print the table of 5 ------");
+		int a=5,i=1;
+		while(i<=10) {
+			System.out.println(a*i);
+			i++;
+		}
+		System.out.println();
+		
+		
+		
+		System.out.println("------ Using do-while loop ------");
+		System.out.println("------ Print the table of 5 ------");
+		int x=5,j=1;
+		do{
+			System.out.println(x*j);
+			j++;
+		}
+		while(j<=10);
+		System.out.println();
+
+	}
+
 }
