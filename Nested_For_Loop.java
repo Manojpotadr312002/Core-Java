05be5a41a7da1940de2db7a5414881b211b3f9d2
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-06 10:19:15 +0530

    Add files via upload

---
+++ Nested_For_Loop.java
@@ -0,0 +1,41 @@
+package ITPreneur;
+
+public class Nested_For_Loop {
+
+	public static void main(String[] args) {
+		
+		// print your name multiple time using nested-loop  
+		// i.e name print 20 times
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=4; j++) {
+				System.out.println("Manoj");
+			}
+		}
+		
+		
+		
+		// print your name multiple time using nested-loop  
+		// i.e name print 20 times
+		for(int i=1; i<=4; i++) {
+			for(int j=1; j<=5; j++) {
+				System.out.println("Manoj");
+			}
+		}
+		
+		
+		
+		// * pattern
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		// Combination or Permutation
+		//for(char i="A"; )
+	}
+
+}
