a4c3f97dd2f1b5eb7fdf2e5ec32c8237c4259735
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-07 10:28:27 +0530

    Nested_While_Loop.java

--- Nested_While_Loop.java
+++ Nested_While_Loop.java
@@ -66,6 +66,25 @@ public class Nested_While_Loop {
 			System.out.println();
 		}
 
+
+		// Prime number
+		int i = 2;
+		while (i <= 100) {
+		    int j = 2;
+		    int count = 0;
+		    while (j < i) {
+		        if (i % j == 0) {
+		           count++;
+		           }
+		           j++;
+		        }
+		    if (count == 0) {
+		        System.out.println(i);
+		    }
+		  
+		    i++;
+		}
+
 	}
 
 }
