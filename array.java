e505747bd5c92b12d1734103e5892136912c52d6
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-09 17:34:27 +0530

    array.java

--- array.java
+++ array.java
@@ -5,37 +5,43 @@ import java.util.Scanner;
 public class array {
 
 	public static void main(String[] args) {
+
+
+		// Insert and Display array element
+		// 1D array
+		int arr[]= new int[5];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the numbers in array: ");
+		for(int i=0; i<5; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0; i<5; i++) {
+			System.out.print(arr[i]+"\t");
+		}
 		
-//		int arr[]= new int[5];
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter the numbers in array: ");
-//		for(int i=0; i<5; i++) {
-//			arr[i] = sc.nextInt();
-//		}
-//		for(int i=0; i<5; i++) {
-//			System.out.print(arr[i]+"\t");
-//		}
 		
-		
-		
-//		int arr[][]= new int[2][3];
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter the numbers in array: ");
-//		for(int i=0; i<2; i++) {
-//			for(int j=0; j<3; j++) {
-//			arr[i][j] = sc.nextInt();
-//			}
-//		}
-//		for(int i=0; i<2; i++) {
-//			for(int j=0; j<3; j++) {
-//			System.out.print(arr[i][j]+"\t");
-//			}
-//			System.out.println();
-//		}
+
+		// Insert and Display array element
+		// 2D array
+		int arr[][]= new int[2][3];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the numbers in array: ");
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<3; j++) {
+			arr[i][j] = sc.nextInt();
+			}
+		}
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<3; j++) {
+			System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
 
 		
 		
-		
+		// Insert and Display array element
+		// 3D array
 		int arr[][][]= new int[2][2][3];
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the numbers in array: ");
