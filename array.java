a037c3538387254dc090d12d9ae62af77238e1ea
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-09 17:25:21 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,62 @@
+package firstpack;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+		
+//		int arr[]= new int[5];
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the numbers in array: ");
+//		for(int i=0; i<5; i++) {
+//			arr[i] = sc.nextInt();
+//		}
+//		for(int i=0; i<5; i++) {
+//			System.out.print(arr[i]+"\t");
+//		}
+		
+		
+		
+//		int arr[][]= new int[2][3];
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the numbers in array: ");
+//		for(int i=0; i<2; i++) {
+//			for(int j=0; j<3; j++) {
+//			arr[i][j] = sc.nextInt();
+//			}
+//		}
+//		for(int i=0; i<2; i++) {
+//			for(int j=0; j<3; j++) {
+//			System.out.print(arr[i][j]+"\t");
+//			}
+//			System.out.println();
+//		}
+
+		
+		
+		
+		int arr[][][]= new int[2][2][3];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the numbers in array: ");
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<2; j++) {
+				for(int k=0; k<3; k++) {
+			       arr[i][j][k] = sc.nextInt();
+				}
+			}
+		}
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<2; j++) {
+				for(int k=0; k<3; k++) {
+			       System.out.print(arr[i][j][k]+"\t");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
