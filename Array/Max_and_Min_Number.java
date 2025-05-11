267c23bc01ae909ef2d1bb6ab0e6c795e140527c
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-11 09:52:56 +0530

    Find Max and Min Number in array

--- Array/Max_and_Min_Number.java
+++ Array/Max_and_Min_Number.java
@@ -1,1 +1,26 @@
+import java.util.*;
+public class array {
 
+	public static void main(String[] args) {
+		
+		
+		//maximum and minimum element in array
+		int n;
+		System.out.print("Enter the size of array:");
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		
+		int a[]=new int[n];
+		for(int i=0;i<n;i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		int max=a[0];
+		for(int i=0;i<n;i++) {
+			if(a[i]>max) {
+				max=a[i];
+			}
+		}
+
+  }
+}
