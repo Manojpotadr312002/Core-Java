b567a48a364d7aceb34b4a858e876b82e0881623
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-12 09:28:13 +0530

    Reverse_Array.java

--- Array/Reverse_Array.java
+++ Array/Reverse_Array.java
@@ -1,2 +1,30 @@
+import java.util.*;
+public class array {
 
+	public static void main(String[] args) {
+		
+		//print the array in reverse direction
+		int n;
+		System.out.print("Enter the size of array: ");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		int a[] = new int[n];
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		System.out.println("Original Array");
+		for(int i=0; i<n; i++) {
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+		
+		System.out.println("After Reverse the Array");
+		for(int i=n-1; i>=0; i--) {
+			System.out.print(a[i]+" ");
+		}
+
+  }
+}
   
