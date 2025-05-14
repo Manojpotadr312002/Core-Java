e11ec2c1290f4b29914889205afccdfcb36de449
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-14 10:38:36 +0530

    Update Prime_Numbers_in_Array.java

--- Array/Prime_Numbers_in_Array.java
+++ Array/Prime_Numbers_in_Array.java
@@ -3,6 +3,35 @@ import java.util.*;
 class PrimeCheck
   {
     public static void main(String args []){
+
+      //print the prime number in array
+		int n;
+		System.out.print("Enter the size of array: ");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		System.out.println("Enter " + n + " elements:");
+		int a[] = new int[n];
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		System.out.println("----- Prime Numbers in Array -----");
+		for(int i=0; i<n; i++) {
+		int x=2; 
+		while(x<=a[i]){
+				if(a[i]%x==0) {
+					break;
+				}
+				x++;
+		      }	
+			
+				if(a[i]==x) {
+					
+					System.out.println(a[i]);
+				
+			}
+		}
       
-    }
-    }
+  }
+  }
