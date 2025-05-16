325222f8e4919c77036e4cf34d054fa2a0eb4dc3
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-16 09:10:24 +0530

    Print Fibonacci series in array

--- Array/Fibonacci_Series_Array.java
+++ Array/Fibonacci_Series_Array.java
@@ -1,1 +1,29 @@
-
+import java.io.*;
+import java.util.*;
+public class Array{
+  public static void main(String args []){
+    //print the fibonacci series in array
+    		int n;
+    		System.out.print("Enter the size of array: ");
+    		Scanner sc = new Scanner(System.in);
+    		n = sc.nextInt();
+    		
+    		System.out.println("Enter the first two numbers: ");
+    		int a[]=new int [n];
+    		for(int i=0; i<2; i++) {
+    			a[i] = sc.nextInt();
+    		}
+    		
+    		
+    		System.out.print(a[0]+" "+a[1]+" ");
+    		
+    		for(int i=2;i<n;i++) {
+    			
+    			a[i]=a[0]+a[1];
+    			a[0]=a[1];
+    			a[1]=a[i];
+    			
+    			System.out.print(a[i]+" ");
+    			}
+  } 
+}
