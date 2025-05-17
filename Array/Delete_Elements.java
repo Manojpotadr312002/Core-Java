a181e097b870978b95e53bd410b92095b65fd4ee
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-17 09:45:02 +0530

    Delete_Elements.java

--- Array/Delete_Elements.java
+++ Array/Delete_Elements.java
@@ -1,1 +1,37 @@
-
+//Delete element in array
+		int n;
+		System.out.print("Enter the size of array: ");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		System.out.print("Enter the "+n+" element of array: ");
+		int a[] = new int[n];
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		System.out.print("Original Array: ");
+		for(int i=0; i<n; i++) {
+			System.out.print(a[i]+" ");
+		}
+		
+		System.out.print("\nEnter the position for delete element of array: ");
+		int position = sc.nextInt();
+		
+		position = position-1;
+		
+		
+		if(position<n) {
+		for(int i=position; i<n-1; i++) {
+			a[i] = a[i+1];
+		}
+		
+		System.out.print("After delete element in array: ");
+		for(int i=0; i<n-1; i++) {
+			System.out.print(a[i]+" ");
+		}
+		}
+		else {
+			System.out.print(" position is invalid... ");
+			
+		}
