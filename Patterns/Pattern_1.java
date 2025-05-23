e939b8ff82db3136299aa524ad90832182398bc4
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-23 12:56:23 +0530

    Add files via upload

---
+++ Patterns/Pattern_1.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class Pattern_1 {
+
+	public static void main(String[] args) {
+		
+		// * pattern
+		int i,j;
+		for(i=1; i<5; i++) {
+			for(j=1; j<5; j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
