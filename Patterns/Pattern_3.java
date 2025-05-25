c8d95b682c7c3551e45a51bcabfdade615f89d7c
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-25 18:19:40 +0530

    Pattern_3.java

--- Patterns/Pattern_3.java
+++ Patterns/Pattern_3.java
@@ -5,5 +5,17 @@ class Pattern_3{
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the value of n : ");
     int n = sc.nextInt();
+
+    for(int i=0; i<n; i++) {
+				for(int space=0; space<n-1-i; space++) {
+					System.out.print(" "+" ");
+				}
+				for(int j=0; j<=2*i; j++) {
+					
+							System.out.print("* ");	
+				}
+				System.out.println();
+			}
+    
   }
 }
