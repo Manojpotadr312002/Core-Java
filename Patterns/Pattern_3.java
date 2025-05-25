115e810ce4a82eccbd71e67ba935c5efa561a268
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-25 18:21:33 +0530

    Pattern_3.java

--- Patterns/Pattern_3.java
+++ Patterns/Pattern_3.java
@@ -7,15 +7,14 @@ class Pattern_3{
     int n = sc.nextInt();
 
     for(int i=0; i<n; i++) {
-				for(int space=0; space<n-1-i; space++) {
-					System.out.print(" "+" ");
-				}
-				for(int j=0; j<=2*i; j++) {
-					
-							System.out.print("* ");	
-				}
-				System.out.println();
-			}
+       for(int space=0; space<n-1-i; space++) {
+	   System.out.print(" "+" ");
+	   }
+	   for(int j=0; j<=2*i; j++) {
+	       System.out.print("* ");	
+		}
+	   System.out.println();
+           }
     
   }
 }
