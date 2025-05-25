9ca7f8bbfbe3dee58be35ca4e3daa4f8864657aa
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-25 18:14:58 +0530

    Pattern_3.java

--- Patterns/Pattern_3.java
+++ Patterns/Pattern_3.java
@@ -1,1 +1,9 @@
-
+import java.io.*;
+import java.util.*;
+class Pattern_3{
+  public static void main(String args []){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of n : ");
+    int n = sc.nextInt();
+  }
+}
