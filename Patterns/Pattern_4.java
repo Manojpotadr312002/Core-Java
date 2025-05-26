adf12913df42ea494e2799344921c140d4a72b19
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-26 18:45:39 +0530

    Pattern_4.java

--- Patterns/Pattern_4.java
+++ Patterns/Pattern_4.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.util.*;
+class Pattern_4{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of n : ");
+    int n = sc.nextInt();
 
+    if(n>5) {
+		for(int i=1; i<n; i++) {
+			for(int j=1; j<n; j++) {
+				if(i==1 || j==1 || i==n-1 || j==n-1 || i==j || i+j==n) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+		}
+		else {
+			System.out.println("Enter the value of n is greater then 5......!");
+		}
+    
+  }
+}
