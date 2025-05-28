8ceb5d225c8da658931320e1b2b7aeb6487e534d
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-28 08:58:15 +0530

    Pattern_6.java

--- Patterns/Pattern_6.java
+++ Patterns/Pattern_6.java
@@ -8,29 +8,29 @@ class Pattern_6{
     int n = sc.nextInt();
 
     if(n%2!=0) {
-			int i, star=1, space=n/2;
-			for(i=0; i<n; i++) {
-				for(int j=0; j<space; j++) {
-					System.out.print(" ");
-				}
-				for(int j=0; j<star; j++) {
-					System.out.print("*");
-				}
-				System.out.println();
+    int i, star=1, space=n/2;
+	for(i=0; i<n; i++) {
+		for(int j=0; j<space; j++) {
+			System.out.print(" ");
+		}
+		for(int j=0; j<star; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
 				
-				if(i<n/2) {
-					space--;
-					star+=2;
-				}
-				else {
-					space++;
-					star-=2;
-				}
-			}
+		if(i<n/2) {
+			space--;
+			star+=2;
 		}
 		else {
-			System.out.println("Enter the odd digit only.....!");
+			space++;
+			star-=2;
 		}
+		}			
+	}
+	else {
+		System.out.println("Enter the odd digit only.....!");
+	}
     
   }
 }
