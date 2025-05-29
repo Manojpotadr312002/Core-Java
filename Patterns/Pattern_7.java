f05665f6904845545b260317ed42ad9369735737
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-05-29 09:32:21 +0530

    Pattern_7.java

--- Patterns/Pattern_7.java
+++ Patterns/Pattern_7.java
@@ -7,24 +7,24 @@ class Pattern_7{
     int n = sc.nextInt();
 
     if(n%2!=0) {
-			int i, star=1;
-			for(i=0; i<n; i++) {
-				for(int j=0; j<star; j++) {
-					System.out.print("*");
-				}
-				System.out.println();
+	int i, star=1;
+	for(i=0; i<n; i++) {
+		for(int j=0; j<star; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
 				
-				if(i<n/2) {
-					star+=1;
-				}
-				else {
-					star-=1;
-				}
-			}
+		if(i<n/2) {
+			star+=1;
 		}
 		else {
-			System.out.println("Enter the odd digit only.....!");
+			star-=1;
 		}
+	}
+     }
+     else {
+	System.out.println("Enter the odd digit only.....!");
+     }
     
   }
 }
