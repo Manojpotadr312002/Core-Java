1c846c73862a3379999607ea35c8ce877be84bca
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-02 09:39:09 +0530

    Update Pattern_10.java

--- Patterns/Pattern_10.java
+++ Patterns/Pattern_10.java
@@ -1,1 +1,26 @@
+import java.io.*;
+import java.util.*;
+class Pattern_10{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of n : ");
+    int n = sc.nextInt();
 
+    int count = 1;
+		char ch = 'a';
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(j%2 != 0) {
+					System.out.print(ch+"\t");
+					ch++;
+				}
+				else {
+					System.out.print(count+"\t");
+					count++;
+				}
+			}	
+			System.out.println();
+		}
+    
+  }
+}
