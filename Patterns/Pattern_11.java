b0ec36505fd18e728386f650343cadaef014fb7b
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-03 10:20:04 +0530

    Update Pattern_11.java

--- Patterns/Pattern_11.java
+++ Patterns/Pattern_11.java
@@ -5,5 +5,23 @@ class Pattern_11{
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the value of n : ");
     int n = sc.nextInt();
+
+    int count = 1;
+		char ch='a';
+	
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n; j++) {
+				if(i%2==0) {
+					System.out.print(count+"\t");
+					count++;
+				}
+				else {
+					System.out.print(ch+"\t");
+					ch++;
+				}
+			}	
+			System.out.println();
+		}
+    
   }
 }
