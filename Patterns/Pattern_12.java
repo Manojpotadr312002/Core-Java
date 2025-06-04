62dbc4acba5199e05a11888ba2a4d439a972ea61
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-04 10:16:37 +0530

    Update Pattern_12.java

--- Patterns/Pattern_12.java
+++ Patterns/Pattern_12.java
@@ -6,7 +6,7 @@ class Pattern_12{
     System.out.println("Enter the value of n : ");
     int n = sc.nextInt();
 
-    if(n%2==0) {
+    if(n%2==0 && n>6) {
 		for(int i=1; i<n; i++) {
 			for(int j=1; j<n; j++) {
 				
@@ -24,7 +24,7 @@ class Pattern_12{
 		}
 		}
 		else {
-			System.out.println("Enter the even digit only.....!");
+			System.out.println("Enter the even digit only and grater than 6");
 		}
     
   }
