c897e8401b39d042f309c580155f988f794f4bfb
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-06 09:21:44 +0530

    Update Pattern_14.java

--- Patterns/Pattern_14.java
+++ Patterns/Pattern_14.java
@@ -5,5 +5,27 @@ class Pattern_14{
     Scanner sc = new Scanner(system.in);
     System.out.println("Enter the value of n : ");
     int n = sc.nextInt();
+
+    if(n%2==0) {
+			for(int i=1; i<n; i++) {
+				for(int j=1; j<n; j++) {
+					
+					if(i==n/2 || j==n/2 || (i==1 && j>n/2) || (i==n-1 && j<n/2) || (j==1 && i<n/2) || (j==n-1 && i>n/2)) {
+					
+							System.out.print("* ");
+							
+						}
+			
+					else {
+						System.out.print(" "+" ");
+					}
+				}
+				System.out.println();
+			}
+			}
+			else {
+				System.out.println("Enter the even digit only.....!");
+			}
+    
   }
 }
