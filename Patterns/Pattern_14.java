acda6a03f66c8594c88c88d7821f054f9c5ef02d
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-06 09:23:53 +0530

    Update Pattern_14.java

--- Patterns/Pattern_14.java
+++ Patterns/Pattern_14.java
@@ -7,25 +7,22 @@ class Pattern_14{
     int n = sc.nextInt();
 
     if(n%2==0) {
-			for(int i=1; i<n; i++) {
-				for(int j=1; j<n; j++) {
-					
-					if(i==n/2 || j==n/2 || (i==1 && j>n/2) || (i==n-1 && j<n/2) || (j==1 && i<n/2) || (j==n-1 && i>n/2)) {
-					
-							System.out.print("* ");
+	for(int i=1; i<n; i++) {
+		for(int j=1; j<n; j++) {
+			if(i==n/2 || j==n/2 || (i==1 && j>n/2) || (i==n-1 && j<n/2) || (j==1 && i<n/2) || (j==n-1 && i>n/2)) {
+				System.out.print("* ");
 							
-						}
-			
-					else {
-						System.out.print(" "+" ");
-					}
-				}
-				System.out.println();
-			}
 			}
 			else {
-				System.out.println("Enter the even digit only.....!");
+				System.out.print(" "+" ");
 			}
+		}
+			System.out.println();
+	}
+	}
+	else {
+		System.out.println("Enter the even digit only.....!");
+	}
     
   }
 }
