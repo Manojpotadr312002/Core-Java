be9097fe167ae64ab95452041cc5498812f57225
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-07 12:54:16 +0530

    Create Pattern_15.java

---
+++ Patterns/Pattern_15.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Pattern_15{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the value of n : ");
+    int n = sc.nextInt();
+    
+  }
+}
