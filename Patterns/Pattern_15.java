56c6f61bb5733823f5791ac8a0619018184dd4b4
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-07 13:02:20 +0530

    Pattern_15.java

--- Patterns/Pattern_15.java
+++ Patterns/Pattern_15.java
@@ -5,6 +5,23 @@ class Pattern_15{
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the value of n : ");
     int n = sc.nextInt();
+
+    if(n%2!=0) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if((i<n/2 && j>n/2)||(i>n/2 && j<n/2)) {
+					System.out.print("  ");
+				}
+				else {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+		}
+		else {
+			System.out.println("Enter the odd number only");
+		}
     
   }
 }
