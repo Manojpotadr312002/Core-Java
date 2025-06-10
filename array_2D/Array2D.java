63d55772a411675721c0fd913f7a4d6f0a61898e
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-10 09:50:49 +0530

    Insert and Display array

--- array_2D/Array2D.java
+++ array_2D/Array2D.java
@@ -1,10 +1,29 @@
 package array_2D;
+import java.io.*;
+import java.util.*;
 
 public class Array2D {
-
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of rows and columns of array : ");
+		int rows = sc.nextInt();
+		int columns = sc.nextInt();
 		
+		int a[][] = new int[rows][columns];
+
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				a[i][j] = sc.nextInt();
+			}
+		}
 		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
 
 	}
 
