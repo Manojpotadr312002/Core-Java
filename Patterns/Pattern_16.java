533cdc6a9d93f1b94ea2c6bc031d5a6a63705dfd
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-10 10:18:00 +0530

    Pattern_16.java

--- Patterns/Pattern_16.java
+++ Patterns/Pattern_16.java
@@ -2,8 +2,12 @@ import java.io.*;
 import java.util.*;
 class Pattern_16{
   public static void main(String args[]){
+    
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the value of n : ");
     int n = sc.nextInt();
+
+    
+    
   }
 }
