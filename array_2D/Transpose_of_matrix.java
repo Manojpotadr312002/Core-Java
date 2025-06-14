c53262b3da7d2fbae88093e12e6c3e2c60367f1b
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-14 10:34:41 +0530

    Transpose_of_matrix

--- array_2D/Transpose_of_matrix.java
+++ array_2D/Transpose_of_matrix.java
@@ -6,5 +6,35 @@ class Array2D{
 		System.out.println("Enter the number of rows and columns of array : ");
 		int rows = sc.nextInt();
 		int columns = sc.nextInt();
+
+	  // Transpose of array
+	
+		System.out.println("Enter the "+rows*columns+" elements in array a: ");
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				System.out.print("Enter the a["+i+"]["+j+"] :");
+				a[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println();
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		System.out.println("Transpose of matrix...");
+		int trr[][]=new int[rows][columns];
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				trr[i][j]=a[j][i];
+				System.out.print(trr[i][j]+" ");
+			}
+			System.out.println();
+		}
   }
 }
