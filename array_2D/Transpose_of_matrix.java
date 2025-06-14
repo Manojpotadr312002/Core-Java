de7f2c024ad44a308d79c4842bdacef7cbeb9910
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-14 10:26:00 +0530

    Transpose_of_matrix.java

---
+++ array_2D/Transpose_of_matrix.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Array2D{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of rows and columns of array : ");
+		int rows = sc.nextInt();
+		int columns = sc.nextInt();
+  }
+}
