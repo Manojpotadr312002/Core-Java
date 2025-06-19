cf0469554df311bb29b96dd689f47239a9d2ae01
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-06-19 10:22:52 +0530

    Pattern_17.java

--- Patterns/Pattern_17.java
+++ Patterns/Pattern_17.java
@@ -7,19 +7,19 @@ class Pattern_17{
     int n = sc.nextInt();
 
     for(int i=0; i<n; i++) {
-			for(int space=0; space<n-1-i; space++) {
-				System.out.print(" "+" ");
-			}
-			for(int j=0; j<=2*i; j++) {
-				if(i==n-1 || j==0 || j==2*i) {
-				System.out.print("* ");	
-				}
-				else {
-					System.out.print("  ");
-				}
-			}
-			System.out.println();
+	for(int space=0; space<n-1-i; space++) {
+		System.out.print(" "+" ");
+	}
+	for(int j=0; j<=2*i; j++) {
+		if(i==n-1 || j==0 || j==2*i) {
+			System.out.print("* ");	
 		}
+		else {
+			System.out.print("  ");
+		}
+	}
+	System.out.println();
+     }
     
   }
 }
