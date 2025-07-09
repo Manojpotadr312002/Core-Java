4773426ae37f78e433d4671ef6830226fa07bbb9
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-09 13:24:30 +0530

    Update Sorting_2DArray.java

--- array_2D/Sorting_2DArray.java
+++ array_2D/Sorting_2DArray.java
@@ -16,7 +16,36 @@ class Array2D{
 				a[i][j] = sc.nextInt();
 			}
 		}
-		
+
+	  	System.out.println("Before sorting array: ");
+			for(int i=0; i<rows; i++) {
+				for(int j=0; j<columns; j++) {
+					System.out.print(a[i][j]+" ");
+				}
+				System.out.println();
+			}
+
+	       //Descending order
+				for(int i=0; i<rows; i++) {
+					for(int j=0; j<columns; j++) 
+						for(int k=0; k<rows; k++) {
+							for(int l=0; l<columns; l++) {
+								if(a[i][j]>a[k][l]) {
+									int t=a[i][j];
+									a[i][j]=a[k][l];
+									a[k][l]=t;
+							}
+						}
+					}
+				}
+				
+				System.out.println("After sorting array: ");
+				for(int i=0; i<rows; i++) {
+					for(int j=0; j<columns; j++) {
+						System.out.print(a[i][j]+" ");
+					}
+					System.out.println();
+				}
 		
   }
 }
