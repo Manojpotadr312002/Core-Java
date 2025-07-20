52c1e9903061b3d88c1369b86a3d67c096b66f50
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-20 11:23:31 +0530

    Update Method_Calling.java

--- Methods/Method_Calling.java
+++ Methods/Method_Calling.java
@@ -2,20 +2,17 @@ import java.util.Scanner;
 
 public class Call_Method {
 	
-	public static void addition(int a, int b) {
-		System.out.println("The addition is :"+(a+b));
+	public static void Personal_Information() {
+		System.out.println("Name : "+"Manoj Sanjay Potdar");
+		System.out.println("Village : "+"Kokrud");
+		System.out.println("Number : "+"9172507101");
 	}
 	
 	public static void main(String[] args) {
 		
 		Operations m = new Operations();
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the value of a and b :");
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		
-		m.addition(a,b);
+		m.Personal_Information();
 		
 	}
 
