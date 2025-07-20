58016401c89601d5db33e3ae3678f718a898a3db
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-20 11:18:22 +0530

    Update Method_Calling.java

--- Methods/Method_Calling.java
+++ Methods/Method_Calling.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Call_Method {
+	
+	public static void addition(int a, int b) {
+		System.out.println("The addition is :"+(a+b));
+	}
+	
+	public static void main(String[] args) {
+		
+		Operations m = new Operations();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a and b :");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		m.addition(a,b);
+		
+	}
+
+}
