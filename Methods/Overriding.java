d7fadec7ea3546dbb1aeee3092bd6dac54f69906
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-22 11:01:36 +0530

    Update Overriding.java

--- Methods/Overriding.java
+++ Methods/Overriding.java
@@ -1,1 +1,23 @@
+class Square{
+	public static void area(int a) {
+		System.out.println("Area of square :"+(a*a));
+	}
+}
 
+class Circle extends Square{
+	public static void area(int r) {
+		System.out.println("Area of circle :"+(3.14*r*r));
+	}
+}
+
+public class Overriding {
+
+	public static void main(String[] args) {
+		
+		Square ob = new Circle();
+		
+		ob.area(4);
+
+	}
+
+}
