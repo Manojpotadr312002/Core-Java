6ce18a4fb151a68c8f1431323a6ecca1c3e893cb
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-23 13:17:49 +0530

    Update Super_Keyword.java

--- Methods/Super_Keyword.java
+++ Methods/Super_Keyword.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.util.*;
 
+class Square{
+	public static void area(int a) {
+		System.out.println("Area of square :"+(a*a));
+	}
+}
+
+class Circle extends Square{
+	public static void area(int r) {
+		System.out.println("Area of circle :"+(3.14*r*r));
+	}
+	
+	void display() {
+		area(4);
+		super.area(4);
+	}
+}
+
+public class Overriding {
+
+	public static void main(String[] args) {
+		
+		Circle ob = new Circle();
+		
+		ob.display();
+		
+	}
+
+}
