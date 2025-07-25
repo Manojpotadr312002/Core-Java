e364dd1ef067a5cad1bef8ebe35e02e7521b0d20
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-25 13:59:15 +0530

    Update Method_Parameter.java

--- Methods/Method_Parameter.java
+++ Methods/Method_Parameter.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Method_Parameter {
+	
+	public static void MyInfo(String name, int age) {
+		System.out.println(name+" is "+age+" years old...!");
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name and age : ");
+		
+		String name = sc.nextLine();
+		int age = sc.nextInt();
+		
+		MyInfo(name,age);
+	}
+
+}
