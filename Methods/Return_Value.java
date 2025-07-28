0dfddc5c58f01afea7c38f4f6ae2749a199e6877
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-28 10:13:44 +0530

    Update Return_Value.java

--- Methods/Return_Value.java
+++ Methods/Return_Value.java
@@ -15,6 +15,10 @@ public class Return_Value {
 	public static void main(String[] args) {
 		
 		System.out.println(myMethod(3));
+		System.out.println(myMethod(2,3));
+		
+		int a = myMethod(5,6,4);
+		System.out.println(a);
 
 	}
 
