869ec209b923a0be550694c02e1ec35bc82736e0
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-07-29 11:49:48 +0530

    Update Recursion.java

--- Methods/Recursion.java
+++ Methods/Recursion.java
@@ -1,17 +1,17 @@
 public class Recursion {
 
-	public static void main(String[] args) {
-		
-		int result = sum(10);
-	    System.out.println(result);
-	  }
-	  public static int sum(int k) {
+	 public static int sum(int k) {
 	    if (k > 0) {
 	      return k + sum(k - 1);
 	    } else {
 	      return 0;
 	    }
-
 	}
 
+	public static void main(String[] args) {
+		
+		int result = sum(10);
+	    System.out.println(result);
+	  }
+	 
 }
