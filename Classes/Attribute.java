27afefd389f1c5621074bd7514587c34b5bdb3f4
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-08-01 14:54:31 +0530

    Update Attribute.java

--- Classes/Attribute.java
+++ Classes/Attribute.java
@@ -1,1 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Attribute {
+  int x = 5;
 
+  public static void main(String[] args) {
+    Attribute myObj1 = new Attribute();  // Object 1
+    Attribute myObj2 = new Attribute();  // Object 2
+    
+    myObj2.x = 25;
+    System.out.println(myObj1.x);  // Outputs 5
+    System.out.println(myObj2.x);  // Outputs 25
+  }
+}
