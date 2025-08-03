e1c85406975f99f90e3729767521d6c2f9942ac2
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-08-03 11:03:59 +0530

    Update Method.java

--- Classes/Method.java
+++ Classes/Method.java
@@ -1,1 +1,13 @@
+public class Method {
+	
+	public static void myStaticMethod() {
+		System.out.println("Static method...!");
+	}
 
+	public static void main(String[] args) {
+		
+		myStaticMethod();
+
+	}
+
+}
