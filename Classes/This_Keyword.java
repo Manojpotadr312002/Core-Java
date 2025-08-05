5343aae6d5408cd0705dc0da3f5a2a1c1df9491b
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-08-05 12:45:57 +0530

    Update This_Keyword.java

--- Classes/This_Keyword.java
+++ Classes/This_Keyword.java
@@ -1,14 +1,13 @@
 public class Main {
-  int x;  // Class variable x
+  int x;  
 
-  // Constructor with one parameter x
-  public Main(int x) {
-    this.x = x; // refers to the class variable x
-  }
+public Main(int x) {
+  this.x = x; 
+}
 
-  public static void main(String[] args) {
-    // Create an object of Main and pass the value 5 to the constructor
-    Main myObj = new Main(5);
-    System.out.println("Value of x = " + myObj.x);
-  }
+public static void main(String[] args) {
+    
+  Main myObj = new Main(5);
+  System.out.println("Value of x = " + myObj.x);
+}
 }
