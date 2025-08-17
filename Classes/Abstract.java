961fe4b1a62093cac5302575b758778097664add
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-08-17 18:36:15 +0530

    Update Abstract.java

--- Classes/Abstract.java
+++ Classes/Abstract.java
@@ -1,1 +1,15 @@
+public class Constructor {
+	int x;
+	
+public Constructor() {
+	x =5;
+}
 
+	public static void main(String[] args) {
+		
+		Constructor obj = new Constructor();
+		System.out.println(obj.x);
+
+	}
+
+}
