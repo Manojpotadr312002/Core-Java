bb05aadc08e00347220ae803c784e07a45be4ce0
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-08-22 19:12:47 +0530

    Add files via upload

---
+++ Strings/Compare.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class Compare {
+
+	public static void main(String[] args) {
+		
+//		String st1 = "Manoj";
+//		String st2 = "Manoj";
+		
+		String st1 = new String ("Manoj");
+		String st2 = new String ("Manoj");
+		
+		if(st1 == st2) {
+			System.out.println("Yes");
+		}
+		else {
+			System.out.println("No");
+		}
+
+	}
+
+}
