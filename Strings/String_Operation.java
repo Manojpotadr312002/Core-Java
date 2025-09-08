e4f8191f452e40771f74047ac0c1e38eae383959
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-09-08 11:31:24 +0530

    Update String_Operation.java

--- Strings/String_Operation.java
+++ Strings/String_Operation.java
@@ -1,1 +1,14 @@
+public class Strings {
 
+	public static void main(String[] args) {
+		
+		String str1 = "Manoj Sanjay Potdar";
+		String str2 = "GANPATI BAPPA MORYA";
+
+    System.out.println(str1.indexOf("Potdar"));
+    System.out.println(str2.indexOf("BAPPA"));
+    System.out.println(str1.indexOf("jay"));
+    
+	}
+
+}
