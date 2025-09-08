7861adc1d7d046fb1dac7e09189b7e1e9ed56d4f
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-09-08 11:23:52 +0530

    Update Length_of_String.java

--- Strings/Length_of_String.java
+++ Strings/Length_of_String.java
@@ -1,1 +1,13 @@
+public class Strings {
 
+	public static void main(String[] args) {
+		
+		String str1 = "Manoj Sanjay Potdar";
+		String str2 = "GANPATI BAPPA MORYA";
+
+    System.out.println(str1.length());
+    System.out.println(str2.length());
+    
+	}
+
+}
