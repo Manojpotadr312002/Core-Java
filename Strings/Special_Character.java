574935d68619fdf48dffe30abaed1532d1919d43
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-09-12 17:56:15 +0530

    Update Special_Character.java

--- Strings/Special_Character.java
+++ Strings/Special_Character.java
@@ -2,7 +2,11 @@ public class Strings {
 
 	public static void main(String[] args) {
 		
+		String txt = "We are the so-called \"Vikings\" from the north.";
+		System.out.println(txt);
 		
+		String txt1 = "It\'s alright.";
+		System.out.println(txt1);
 
 	}
 
