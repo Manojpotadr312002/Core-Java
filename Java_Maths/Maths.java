abc6b577d4f56a7f4ed25c68bd6d11071f7658f3
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-09-17 20:15:41 +0530

    Update Maths.java

--- Java_Maths/Maths.java
+++ Java_Maths/Maths.java
@@ -6,11 +6,13 @@ public class Maths {
 
 		System.out.println(Math.max(5, 10)); 
 		System.out.println(Math.min(5, 10)); 
-		System.out.println(Math.sqrt(64)); 
+		System.out.println(Math.sqrt(64));
+		System.out.println(Math.abs(-4.7));
 
 	}
 
 }
+
 
 
 
