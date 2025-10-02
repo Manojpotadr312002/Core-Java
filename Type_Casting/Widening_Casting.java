dba2cf63a21b3b2e5269af954c133656541cc5c0
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-10-02 13:10:11 +0530

    Update Widening_Casting.java

--- Type_Casting/Widening_Casting.java
+++ Type_Casting/Widening_Casting.java
@@ -3,8 +3,8 @@ package Type_Casting;
 public class Widening_Casting {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- int myInt = 9;
+		
+    int myInt = 9;
     double myDouble = myInt; // Automatic casting: int to double
 
     System.out.println(myInt);
@@ -12,4 +12,5 @@ public class Widening_Casting {
 	}
 
 }
+
 
