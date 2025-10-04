6b1fc2376f00116cda47fbe4049014f7cbe0f42b
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-10-04 15:59:55 +0530

    Update Widening_Casting.java

--- Type_Casting/Widening_Casting.java
+++ Type_Casting/Widening_Casting.java
@@ -5,12 +5,13 @@ public class Widening_Casting {
 	public static void main(String[] args) {
 		
     int myInt = 9;
-    double myDouble = myInt; // Automatic casting: int to double
+    double myDouble = myInt; 
 
     System.out.println(myInt);
     System.out.println(myDouble);
 	}
 
 }
+
 
 
