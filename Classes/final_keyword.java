b37511e5cb7fadb293976bd0c7788a47179db96b
Manoj Potdar <168889820+Manojpotadr312002@users.noreply.github.com>
2025-12-10 15:24:33 +0530

    Update final_keyword.java

--- Classes/final_keyword.java
+++ Classes/final_keyword.java
@@ -13,7 +13,7 @@ public class Attributes {
 		Attributes myObj2 = new Attributes();
 		
 		myObj1.x = 25;
-	/*	myObj2.y = 40;  // will generate an error: cannot assign a value to a final variable  */
+	
 		
 		System.out.println(myObj1.x);
 		System.out.println(myObj2.y);
